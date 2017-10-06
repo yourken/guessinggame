4a5837588e2e9ed58fe494333d9a3f6bf3b61e64
jay <jay@debian.sf.de>
2017-10-06 16:23:45 +0200

    game ready to play...

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,2 +1,29 @@
-test
-\ntest
+#!/usr/bin/env bash
+# File: guessgame.sh
+
+numFiles=$(ls -1|wc -l)
+guessedNum=-1
+
+function input
+{
+	echo "Guess the number of files in this dir:"
+	read guessedNum
+}
+
+while true; 
+do
+	input
+
+	if [[ $numFiles -eq $guessedNum ]]
+	then
+		echo -e "You are right!\nWell done!"
+		break;
+
+	elif [[ $numFiles -gt $guessedNum ]]	
+	then
+		echo "Your guess was too low..."
+	else
+		echo "Your guess was too high..."
+	fi
+done
+
